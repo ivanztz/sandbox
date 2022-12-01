aa9422b723e7f36de6ce113154a9b9b8d80d4384
ivan_zayats <ivan_zayats@rntgroup.com>
2022-12-01 10:53:22 +0300

    Schema provisioning improved. Incremental provisioning added

--- docker/config/schema/schemaregistry_provision.sh
+++ docker/config/schema/schemaregistry_provision.sh
@@ -3,39 +3,35 @@
 echo "Checking schema registry status at $SCHEMA_REGISTRY"
 
 status=undefined
-while [  "$status" == "undefined" ];
-do
-  response=$(curl $SCHEMA_REGISTRY/subjects)
-  echo "Check result: $response"
+while [ "$status" == "undefined" ]; do
+  subjects=$(curl $SCHEMA_REGISTRY/subjects)
+  echo "Check result: $subjects"
 
-  if [ "$response" == "[]" ]
-  then
-    status="provision_required"
+  if [[ "$subjects" =~ ^[\[].*[\]]$ ]]; then
+    status="provision"
   else
-    if [[ "$response" =~ \[.+\] ]]
-    then
-      status="provision_not_required"
-    else
-      sleep 1
-    fi
+    echo "Waiting for schema registry is up"
+    sleep 1
   fi
 done
 
-if [ $status == "provision_required" ]
-then
+if [ $status == "provision" ]; then
   echo "Starting importing schemas from /etc/schema/import"
-  for schema in $(ls /etc/schema/import *.avro)
-  do
+  for schema in $(find /etc/schema/import -type f -name "*.avsc"); do
     echo "Importing schema $schema to $SCHEMA_REGISTRY"
     # extracting subject value from avro file
-    subject=$(cat /etc/schema/import/$schema | grep subject | awk -F"\"" '{print $4}')
+    subject=$(cat $schema | grep subject | awk -F"\"" '{print $4}')
 
-    # building schema request string from avro file. Sed replaces new lines and escapes backslashes and double quotes
-    data="{\"schema\":\"$(cat /etc/schema/import/$schema | tr -d '\n' | sed 's/\n//g; s/\\/\\\\/g; s/"/\\"/g')\", \"schemaType\": \"AVRO\"}"
-    echo "Creating schema for subject $subject : $data"
+    if [[ $subjects == *$subject* ]]; then
+      echo "Subject $subject is already provisioned. Skipping execution"
+    else
+      # building schema request string from avro file. Sed replaces new lines and escapes backslashes and double quotes
+      data="{\"schema\":\"$(cat $schema | tr -d '\n' | sed 's/\n//g; s/\\/\\\\/g; s/"/\\"/g')\", \"schemaType\": \"AVRO\"}"
+      echo "Provisioning schema for subject $subject : $data"
 
-    result=$(curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" --data "$data"  $SCHEMA_REGISTRY/subjects/$subject/versions)
-    echo "Result for $subject: $result"
+      result=$(curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" --data "$data" $SCHEMA_REGISTRY/subjects/$subject/versions)
+      echo "Result for $subject: $result"
+    fi
   done
 
   echo "Finishing import"
