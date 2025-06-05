54a77850127292709401195acb83bf63212263b4
ivan_zayats <ivan_zayats@rntgroup.com>
2025-06-05 09:53:43 +0300

    Changes: - Updated feign configuration to make it follow official documentation

--- object-service/src/main/java/com/iz/sandbox/feign/EventsApiClient.java
+++ object-service/src/main/java/com/iz/sandbox/feign/EventsApiClient.java
@@ -3,6 +3,6 @@ package com.iz.sandbox.feign;
 import com.iz.sandbox.object.event.api.EventsApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "events",  url = "${clients.events}")
+@FeignClient(value = "events")
 public interface EventsApiClient extends EventsApi {
 }
